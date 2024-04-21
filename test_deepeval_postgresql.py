8c1d89d694fc8e9c06d016b8636017d78a5094df
Talari Pavan Kumar <95948550+talaripavan@users.noreply.github.com>
2024-04-21 11:35:28 +0530

    Updated code.

--- test_deepeval_postgresql.py
+++ test_deepeval_postgresql.py
@@ -1,55 +1,70 @@
-import json
-import psycopg2
+import psycopg2 , json
 
-with open('test1.json', 'r') as file:
-    json_data = json.load(file)
+class DeepEvalDatabase:
+    def __init__(self,hostname,database,username,password,port):
+        self.hostname = hostname
+        self.database = database
+        self.username = username
+        self.password = password
+        self.port = port
+        
+    def connection(self):
+        try:
+            self.conn = psycopg2.connect(
+                host = self.hostname,
+                dbname = self.database,
+                user = self.username,
+                password = self.password,
+                port = self.port
+            )
+            self.cur = self.conn.cursor()
+            print("Connected to the database")
+        except psycopg2.Error as e:
+            print("Error in connecting ",e)
+            
+    def insert_data(self):
+        # First we are reading the json file.
+        with open('test_10.json','r') as file:
+            json_data = json.load(file)
+            
+        testfile = json_data['testFile']
+        deployment = json_data['deployment']
+        testcase = json.dumps(json_data['testCases'])
+        
+        insert_statements = '''
+            INSERT INTO test_schema.deepeval1(testfile, deployment, testcase)
+            VALUES (%s,%s,%s);
+        '''
+        try:
+            self.cur.execute(insert_statements,(testfile,deployment,testcase))
+            self.conn.commit()
+            print("Data Inserted Successfully")
+        except psycopg2.Error as e:
+            print("Error in inserting data : ",e)
+            
+    def read_data(self):
+        try:
+            self.cur.execute('SELECT * FROM test_schema.deepeval1')
+            rows = self.cur.fetchall()
+            print("Data retrived from the table :-")
+            for row in rows:
+                print(row)
+        except psycopg2.Error as e:
+            print("Can not retrive the data from table", e)
+            
 
 
-hostname = 'localhost'
-database = 'Demo_connect'
-username = '<user name>'
-pwd = '<your password>'
-port_id = 5432
-
-conn = psycopg2.connect(
-    host=hostname,
-    dbname=database,
-    user=username,
-    password=pwd,
-    port=port_id
-)
-
-
-cur = conn.cursor()
-
-
-create_script = '''
-    CREATE TABLE deepeval(
-        id UUID DEFAULT gen_random_uuid(),
-        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-        testfile TEXT,
-        deployment BOOLEAN,
-        testcase JSON
-    );
-'''
-
-
-cur.execute(create_script)
-
-
-insert_statement = '''
-    INSERT INTO deepeval(testfile, deployment, testcase)
-    VALUES (%s, %s, %s);
-'''
-
-
-testfile = json_data['testFile']
-deployment = json_data['deployment']
-testcase = json.dumps(json_data['testCases'])  
-
-cur.execute(insert_statement, (testfile, deployment, testcase))
-
-conn.commit()
-
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    
+    hostname = 'localhost'
+    database = '<Database>'
+    username = '<username>'
+    password = '<password>'
+    port = 5432
+    
+    deepeval_db = DeepEvalDatabase(hostname,database,username,password,port)
+    
+    # Calling the methods.
+    deepeval_db.connection()
+    deepeval_db.insert_data()
+    deepeval_db.read_data()
