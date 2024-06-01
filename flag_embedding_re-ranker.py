1cf40cf3053d88a3d4b3b71e01bc0bef1b9fbe9b
Talari Pavan Kumar <95948550+talaripavan@users.noreply.github.com>
2024-06-02 02:32:18 +0530

    Flag Embedding Re-Ranker

---
+++ flag_embedding_re-ranker.py
@@ -0,0 +1,26 @@
+import os
+from llama_index.vector_stores.milvus import MilvusVectorStore
+from llama_index.core import VectorStoreIndex , get_response_synthesizer
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.postprocessor.flag_embedding_reranker import FlagEmbeddingReranker
+
+collection_name = os.getenv('COLLECTION_NAME')
+vector_store = MilvusVectorStore(collection_name=collection_name, dim=1536, overwrite=False, uri="http://192.168.29.44:19530")
+index = VectorStoreIndex.from_vector_store(vector_store)
+retriever = VectorIndexRetriever(index=index,similarity_top_k=3)
+response_synthesizer = get_response_synthesizer()
+flag_rerank = FlagEmbeddingReranker(model="BAAI/bge-reranker-large", top_n=5)
+
+retriever_rerank = RetrieverQueryEngine(
+        retriever=retriever,
+        response_synthesizer=response_synthesizer,
+        node_postprocessors=[flag_rerank]
+)
+
+retriever_rerank_response = retriever_rerank.query(" What is the full name for the college code 'ACEG' ? ")
+print(retriever_rerank_response)
+print("Retriever Response with Re-Rank",retriever_rerank_response.get_formatted_sources(length=200))
+
+# It takes a lot of time to run .
